4756acae10002c76baf571a43fb7e1f68a811851
danleonca <danleon022688@gmail.com>
2020-08-20 19:22:57 -0500

    Updates

---
+++ precache-manifest.8a34bfb6360dd92a33c81de7fd59201d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73117a601d6f7a7f15bee026fb0270b",
+    "url": "/miportafolio/index.html"
+  },
+  {
+    "revision": "45991358c6be2e308c39",
+    "url": "/miportafolio/static/css/main.5d9d98c9.chunk.css"
+  },
+  {
+    "revision": "dcf078df77263f418b4a",
+    "url": "/miportafolio/static/js/2.f87900d3.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/miportafolio/static/js/2.f87900d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45991358c6be2e308c39",
+    "url": "/miportafolio/static/js/main.fa576f47.chunk.js"
+  },
+  {
+    "revision": "d9b241afaa8b7dfbdd95",
+    "url": "/miportafolio/static/js/runtime-main.cc31f301.js"
+  },
+  {
+    "revision": "2a7eca7e6fca915c078b0b70f6fd8243",
+    "url": "/miportafolio/static/media/bosqueInv.2a7eca7e.jpg"
+  },
+  {
+    "revision": "ee0a29dd1ccabbf4a6e0cc13367a7754",
+    "url": "/miportafolio/static/media/burgerQ.ee0a29dd.svg"
+  },
+  {
+    "revision": "5ed5f39eb29a4d03b83ff7d31a8103b0",
+    "url": "/miportafolio/static/media/fatverine.5ed5f39e.png"
+  },
+  {
+    "revision": "4514eb0c10a37c4da6741608348aa490",
+    "url": "/miportafolio/static/media/mdLinks.4514eb0c.svg"
+  },
+  {
+    "revision": "fa02a0e0242ddd4264fcc6f5c37758ad",
+    "url": "/miportafolio/static/media/pokemon.fa02a0e0.svg"
+  },
+  {
+    "revision": "43929d9620c35bf2292fec1a77295e1e",
+    "url": "/miportafolio/static/media/socialNet.43929d96.svg"
+  }
+]);
